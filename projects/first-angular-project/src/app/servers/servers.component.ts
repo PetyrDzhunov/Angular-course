64774b24042cd7bd1e591920a362aaf4af21b11c
PetyrDzhunov <petar.djunov@abv.bg>
2022-06-27 14:05:23 +0300

    basic event binding

--- projects/first-angular-project/src/app/servers/servers.component.ts
+++ projects/first-angular-project/src/app/servers/servers.component.ts
@@ -7,12 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
-
+  serverCreationStatus= "No server was created"
   constructor() { 
    
   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server was created!"
   }
 
 }
