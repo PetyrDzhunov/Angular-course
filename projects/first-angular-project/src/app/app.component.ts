f163aa7acb2e42c0b18370cd131bec2d8835bae2
PetyrDzhunov <petar.djunov@abv.bg>
2022-06-27 15:22:23 +0300

    finish the angular basics section

--- projects/first-angular-project/src/app/app.component.ts
+++ projects/first-angular-project/src/app/app.component.ts
@@ -3,12 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles :[`
-    h3 {
-      color:red;
-    }
-  `]
+  styleUrls:['./app.component.css']
 })
 export class AppComponent {
   title = 'first-angular-project';
+  showSecret=false;
+  log=[] as number[]
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
 }
--- projects/first-angular-project/src/app/servers/servers.component.ts
+++ projects/first-angular-project/src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent implements OnInit {
   serverName = 'test server';
   username= '';
   serverCreated = false;
+  servers=['testServer','testServer2']
   constructor() { 
    
   }
@@ -20,6 +21,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = "Server was created! Name is " + this.serverName;
+    this.servers.push(this.serverName)
     this.serverCreated=true;
   }
 
