c8d091bf4578cdcd4cb2ba8b0be9926c91025b65
Petar Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-07-01 16:16:37 +0300

    the wild card route catching all other routes to display 404 page

--- projects/routing/src/app/app-routing.module.ts
+++ projects/routing/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersComponent } from './servers/servers.component';
@@ -16,6 +17,7 @@ const routes: Routes = [
     {path:':id',component:ServerComponent},
     {path:':id/edit',component:EditServerComponent},
   ]},
+  {path:"**",component:PageNotFoundComponent},
  
 
 ];  
