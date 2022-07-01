a715c105bf26dcf06ec0407f2d5b90404ee9fb59
Petar Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-07-01 14:40:04 +0300

    change data dynamically whenever the url changes

--- projects/routing/src/app/app-routing.module.ts
+++ projects/routing/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ServerComponent } from './servers/server/server.component';
+import { UserComponent } from './users/user/user.component';
 import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'users',component:UsersComponent},
-  {path:'servers',component:ServerComponent}
+  {path:'users/:id/:name',component:UserComponent},
+  {path:'servers',component:ServerComponent},
 ];
 
 @NgModule({
