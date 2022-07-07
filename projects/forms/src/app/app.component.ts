3c151a4499ac1e103f16b46b5462111a0f2b8e2f
Petar Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-07-07 14:03:03 +0300

    reset form

--- projects/forms/src/app/app.component.ts
+++ projects/forms/src/app/app.component.ts
@@ -45,6 +45,7 @@ export class AppComponent {
     this.user.secretQuestion=this.signUpForm.value.secret
     this.user.answer=this.signUpForm.value.questionAnswered
     this.user.gender=this.signUpForm.value.gender
+    this.signUpForm.reset()
   }
 
 }
