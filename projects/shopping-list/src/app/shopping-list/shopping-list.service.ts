aa4c0d775f9479b0d209a890e3e4430f1db7064f
Petar Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-07-08 15:40:47 +0300

    rework the forms

--- projects/shopping-list/src/app/shopping-list/shopping-list.service.ts
+++ projects/shopping-list/src/app/shopping-list/shopping-list.service.ts
@@ -3,6 +3,7 @@ import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
     ingredientsChanged = new Subject<Ingredient[]>()
+    startedEditing = new Subject<number>()
 
     private ingredients:Ingredient[] = [
         new Ingredient('tomatoes',4),
@@ -13,8 +14,22 @@ export class ShoppingListService {
         return this.ingredients.slice();
       }
 
+      getIngredient(index:number) {
+        return this.ingredients[index];
+      }
+
+      updateIngredient(index:number,newIngredient:Ingredient) {
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice())
+      }
+
       addIngridient(ingredient:Ingredient) {
         this.ingredients.push(ingredient)
+        this.ingredientsChanged.next(this.ingredients.slice())
+      }
+
+      deleteIngredient(index:number) {
+        this.ingredients.splice(index,1);
         this.ingredientsChanged.next(this.ingredients.slice())
       }
 
